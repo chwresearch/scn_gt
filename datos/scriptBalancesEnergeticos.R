0d4d7ba94d1da0706f3cc4d5cef02400defb33c6
Renato Vargas <renovargas@gmail.com>
2022-09-11 14:54:05 -0600

    uPDATES

--- datos/scriptBalancesEnergeticos.R
+++ datos/scriptBalancesEnergeticos.R
@@ -52,6 +52,7 @@ for (i in 1:length(hojas)){
   ))
   
   balance[is.na(balance)] <- 0.0 # cambiamos "sin dato" a cero por conveniencia 
+  balance[c(3,16,17,18),] <- balance[c(3,16,17,18),] * (-1) # cambiamos los negativos
   rownames(balance) <- c(sprintf("bf%03d", seq(1, dim(balance)[1])))
   colnames(balance) <- c(sprintf("bc%03d", seq(1, dim(balance)[2])))
   
