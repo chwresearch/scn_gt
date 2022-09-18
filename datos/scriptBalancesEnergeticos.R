6d04d99d99bce38d5786e961dd9ad4d68dea7e15
Renato Vargas <renatovargas@users.noreply.github.com>
2022-09-18 11:09:20 -0600

    balances listos

--- datos/scriptBalancesEnergeticos.R
+++ datos/scriptBalancesEnergeticos.R
@@ -52,7 +52,7 @@ for (i in 1:length(hojas)){
   ))
   
   balance[is.na(balance)] <- 0.0 # cambiamos "sin dato" a cero por conveniencia 
-  balance[balance[c(7:14),]>0] <- 0 # cambiamos los positivos que no suman a la oferta
+  balance[c(7:14),][balance[c(7:14),]>0]  <- 0 # cambiamos los positivos que no suman a la oferta
   balance[c(4,7:14),] <- balance[c(4,7:14),] * (-1) # cambiamos los negativos
   rownames(balance) <- c(sprintf("bf%03d", seq(1, dim(balance)[1])))
   colnames(balance) <- c(sprintf("bc%03d", seq(1, dim(balance)[2])))
